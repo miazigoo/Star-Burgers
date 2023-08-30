4c163498e688a616643eb00bce0024364dedbb07
miazigoo <kasper6024@gmail.com>
2023-08-31 00:10:42 +0300

    Упрощен код

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -88,7 +88,7 @@ def product_list_api(request):
 @api_view(['POST'])
 def register_order(request):
     serializer = OrderSerializer(data=request.data)
-    if serializer.is_valid(raise_exception=True):
+    if serializer.is_valid():
         serializer.create(serializer.validated_data)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     else:
