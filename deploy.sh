27f198bcf29b47c36395b673406918f4f1722706
root <root@cv4102119.novalocal>
2024-09-22 19:59:55 +0300

    Add js comand

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,7 @@ git pull
 source venv/bin/activate
 python manage.py migrate --noinput
 npm ci
+./node_modules/.bin/parcel bundles-src/index.js --dist-dir bundles --public-url="./"
 
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
